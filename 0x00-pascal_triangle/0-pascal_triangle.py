2a353ed319497ba1489259dbab59d09c7221f1b4
Hasla <richkibz@gmail.com>
2023-06-30 00:23:18 +0300

    3 major steps to create a pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+""" pascal triangle"""
+
 
 def pascal_triangle(n):
+    """main  function of the project"""
 
     lst = [[1]*(i+1) for i in range(n)]
 
-    if (n <= 0): 
-        return ([])
-    
+    if (n <= 0):
+        return []
+
     for i in range(n):
         for j in range(1, i):
             lst[i][j] = lst[i-1][j-1] + lst[i-1][j]
-    return lst  
+    return lst
